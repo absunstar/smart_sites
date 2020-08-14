7a4eb410712923da9183b6ead8aea449b04b2d12
absunstar <absunstar@gmail.com>
2020-08-14 12:03:41 +0000

    #

--- icar.js
+++ icar.js
@@ -1,7 +1,7 @@
 const site = require('isite')({
     port: [80 , 33001],
     lang: 'ar',
-    version : '1.0.10',
+    version : '1.0.11',
     name: 'icar',
     theme: 'theme_paper',
     mongodb: {
