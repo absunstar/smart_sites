7fc44b87bea0bcaecb858198967f165fb322256d
absunstar <absunstar@gmail.com>
2022-05-06 10:56:05 +0200

    update

--- icar.js
+++ icar.js
@@ -1,54 +1,56 @@
 const site = require('isite')({
-    port: [80 , 33001],
-    lang: 'ar',
-    version : '1.0.12',
-    name: 'icar',
-    theme: 'theme_paper',
-    mongodb: {
-        db: 'smart_code_icar',
-        limit: 100000
+  port: [80, 33001],
+  lang: 'ar',
+  version: '1.0.13',
+  name: 'icar',
+  theme: 'theme_paper',
+  mongodb: {
+    db: 'smart_code_icar',
+    limit: 100000,
+    identity: {
+      enabled: !0,
     },
-    security: {
-        keys: ['e698f2679be5ba5c9c0b0031cb5b057c' , '9705a3a85c1b21118532fefcee840f99'],
-      }
-})
+  },
+  security: {
+    keys: ['e698f2679be5ba5c9c0b0031cb5b057c', '9705a3a85c1b21118532fefcee840f99'],
+  },
+});
 
 site.get({
-    name: '/',
-    path: site.dir + '/'
-})
+  name: '/',
+  path: site.dir + '/',
+});
 
 site.get({
-    name: '/',
-    path: __dirname +'/apps_icar/0/site_files/html/index.html',
-    parser: 'html css js'
-})
+  name: '/',
+  path: __dirname + '/apps_icar/0/site_files/html/index.html',
+  parser: 'html css js',
+});
 
-site.words.add({
-    "name": "le",
-    "en": "Ryal",
-    "ar": "ريال"
-}, {
-    "name": "pound",
-    "en": "Ryal",
-    "ar": "ريال"
-})
+site.words.add(
+  {
+    name: 'le',
+    en: 'Ryal',
+    ar: 'ريال',
+  },
+  {
+    name: 'pound',
+    en: 'Ryal',
+    ar: 'ريال',
+  }
+);
 
-site.loadLocalApp('client-side')
-site.importApps(__dirname + '/apps_icar')
-site.importApp(__dirname + '/apps_private/security')
-site.importApp(__dirname + '/apps_private/ui-print')
-site.importApp(__dirname + '/apps_private/ui-help')
+site.loadLocalApp('client-side');
+site.importApps(__dirname + '/apps_icar');
+site.importApp(__dirname + '/apps_private/security');
+site.importApp(__dirname + '/apps_private/ui-print');
+site.importApp(__dirname + '/apps_private/ui-help');
 
-site.importApps(__dirname + '/apps_core')
-site.addFeature('icar')
+site.importApps(__dirname + '/apps_core');
+site.addFeature('icar');
 
-
-
-site.run()
-
+site.run();
 
 // site.on('zk attend', attend=>{
 //     console.log(attend)
 // })
-
